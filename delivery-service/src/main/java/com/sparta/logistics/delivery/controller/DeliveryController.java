b943ea6e3eefe0c3029a8adfa2cc49466910695d
look516 <happycatcindy@gmail.com>
2026-05-22 12:08:31 +0900

    feat: 배송 목록 조회 Controller (#98)

--- delivery-service/src/main/java/com/sparta/logistics/delivery/controller/DeliveryController.java
+++ delivery-service/src/main/java/com/sparta/logistics/delivery/controller/DeliveryController.java
@@ -1,14 +1,18 @@
 package com.sparta.logistics.delivery.controller;
 
+import com.sparta.logistics.common.response.ApiResponse;
 import com.sparta.logistics.delivery.dto.DeliveryDetailResponse;
 import com.sparta.logistics.delivery.service.DeliveryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -18,6 +22,8 @@ public class DeliveryController {
 
     private final DeliveryService deliveryService;
 
+    // TODO: 스웨거
+    // 배송 상세 조회
     @GetMapping("/{deliveryId}")
     public ResponseEntity<DeliveryDetailResponse> getDelivery(
             @PathVariable UUID deliveryId
@@ -25,6 +31,19 @@ public class DeliveryController {
         DeliveryDetailResponse response = deliveryService.getDelivery(deliveryId);
 
         return ResponseEntity.ok(response);
+    }
 
+    // TODO: 배송 생성 API
+
+    // 배송 목록 조회
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<DeliveryDetailResponse>>> getDeliveries(
+            @PathVariable UUID deliveryId,
+            // TODO: AUTH
+
+    ) {
+        List<DeliveryListResponse> responses = deliveryService.getDeliveriesByRole(deliveryId, );
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(responses));
     }
 }
